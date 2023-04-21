3f2f15272cb21e5eda1f3ae7d179a7ded315d93e
Lường Dũng <ole.phan.10@gmail.com>
2023-04-22 04:42:29 +0700

    update code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,7 +11,7 @@ import { Router, NavigationEnd } from "@angular/router";
   styleUrls: ["./app.component.scss"],
 })
 export class AppComponent {
-  title = "bitakon";
+  title = "Ulink";
   themeSetting: boolean = false;
 
   handleThemeSetting() {
